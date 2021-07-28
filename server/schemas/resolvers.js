092bf13364ed08c14a65a6163dc2a7ae3a8bc2f5
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-07-28 17:44:35 -0400

    Added basic structure to resolvers and typeDefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,16 @@
+const { AuthError } = require('apollo-server-express')
+const { User } = require('../models')
+const { signToken } = require('../utils/auth')
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({__id: context.user._id})
+      }
+      throw new AuthError('You need to log in.')
+    },
+  }
+}
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,14 @@
+const { gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`
+
+module.exports = typeDefs
