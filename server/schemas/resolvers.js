110b9c03e3b19de8b29c34858f598ac58d3a4953
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-07-28 18:16:55 -0400

    Coded queries and mutations on resolvers file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,10 +6,61 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({__id: context.user._id})
+        return User.findOne({__id: context.user._id}).select('-__v -password')
       }
       throw new AuthError('You need to log in.')
     },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const newUser = await User.create(args)
+      const token = signToken(newUser)
+
+      return { token, newUser}
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email })
+
+      if (!user) {
+        throw new AuthError('User not found')
+      }
+
+      const correctPw = await user.isCorrectPassword(password)
+
+      if (!correctPw) {
+        throw new AuthError('Credentials do not match!')
+      }
+
+      const token = signToken(user)
+
+      return { token, user }
+    },
+
+    saveBook: async (parent, { book }, context) => {
+      if (context.user) {
+        const user = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: book }},
+          { new: true }
+        )
+
+        return user
+      };
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const user = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId }}},
+          { new: true }
+        )
+
+        return user
+      };
+    }
   }
 }
 
