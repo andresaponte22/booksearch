6266b7af1517e3426fe5817028cca7c4821c5996
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-07-28 19:33:38 -0400

    Coded mutations loginUser, addUser, saveBook and remove book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,3 +1,59 @@
 // import
+import { gql } from '@apollo/client'
 
 // mutations, login, add, save, remove
+export const LOGIN_USER = gql`
+  mutation login($email: String!, password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, password: String!) {
+    login(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: BookInput!) {
+    saveBook(book: $book) {
+      username
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      username
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`
