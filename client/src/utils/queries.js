98277aaca59aa12218fd919f1d0d17070580e01f
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-07-28 18:56:00 -0400

    Coded gql queries in front end

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,21 @@
 // import
+import { gql } from "@apollo/client"
+
 // queries
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`
