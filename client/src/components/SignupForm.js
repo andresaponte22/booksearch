77ba0c402dd9658cefebcf81ba1a8519df26639b
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-07-28 20:54:43 -0400

    Added mutation functionality to loginform.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 // import { createUser } from '../utils/API';
@@ -20,6 +20,8 @@ const SignupForm = () => {
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  const [addUser] = useMutation(ADD_USER)
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -31,15 +33,19 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      // const response = await createUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // const { token, user } = await response.json();
+      // console.log(user);
+      // Auth.login(token);
+      const { data } = await addUser({
+        variables: { ...userFormData }
+      })
+      Auth.login(data.addUser.token)
     } catch (err) {
       console.error(err);
       setShowAlert(true);
